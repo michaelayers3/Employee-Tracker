97e6d31d68023a05e65b7ebdfc2abe788cc46f8a
michaelayers3 <michaelgregoryayers@gmail.com>
2023-05-10 13:59:22 -0600

    began framework for server.js. created tables in schema.sql. will begin testing to make sure I put the tables in correctly, create seed data to populate said tables, then begin working on the inquirer portion.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlendcoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user:'root',
+        password: 'password',
+        database: 'employee_db'
+
+    },
+    console.log('Connected to the employee database.')
+);
+

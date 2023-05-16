a58dad54aad40f1ebe3c99000b5543e7c8919d91
michaelayers3 <michaelgregoryayers@gmail.com>
2023-05-16 11:31:30 -0600

    final touches. Want to see if I can format some stuff a little better in office hours, but it's functional and meets all the criteria.

--- server.js
+++ server.js
@@ -19,24 +19,29 @@ const db = mysql.createConnection(
     console.log('Connected to the employee database.')
 );
 
-//     console.log(`
-// ======================================================
-//  _______             _                       
-// (_______)           | |                                           
-//  _____   ____  ____ | | ___  _   _  ____ ____    
-// |  ___) |    \|  _ \| |/ _ \| | | |/ _  ) _  )  
-// | |_____| | | | | | | | |_| | |_| ( (/ ( (/ /       
-// |_______)_|_|_| ||_/|_|\___/ \__  |\____)____)    
-//               |_|           (____/                                                  
-//  ______                 
-// |  ___ \      
-// | | _ | | ____ ____   ____  ____  ____  ____      
-// | || || |/ _  |  _ \  / _  |/ _  |/ _  )/ ___) 
-// | || || ( ( | | | | ( ( | ( ( | ( (/ /| |     
-// |_||_||_|\_||_|_| |_|\_||_|\_|| |\____)_|   
-//                           (_____|
-// ======================================================
-//    `);
+    console.log(`
+======================================================
+╔═══╗        ╔╗                  
+║╔══╝        ║║                  
+║╚══╗╔╗╔╗╔══╗║║ ╔══╗╔╗ ╔╗╔══╗╔══╗
+║╔══╝║╚╝║║╔╗║║║ ║╔╗║║║ ║║║╔╗║║╔╗║
+║╚══╗║║║║║╚╝║║╚╗║╚╝║║╚═╝║║║═╣║║═╣
+╚═══╝╚╩╩╝║╔═╝╚═╝╚══╝╚═╗╔╝╚══╝╚══╝
+         ║║         ╔═╝║         
+         ╚╝         ╚══╝                                                          
+
+╔═╗╔═╗                         
+║║╚╝║║                         
+║╔╗╔╗║╔══╗ ╔═╗ ╔══╗ ╔══╗╔══╗╔═╗
+║║║║║║╚ ╗║ ║╔╗╗╚ ╗║ ║╔╗║║╔╗║║╔╝
+║║║║║║║╚╝╚╗║║║║║╚╝╚╗║╚╝║║║═╣║║ 
+╚╝╚╝╚╝╚═══╝╚╝╚╝╚═══╝╚═╗║╚══╝╚╝ 
+                    ╔═╝║       
+                    ╚══╝       
+
+======================================================
+   `);
+  
 inquirer
 .prompt([
     {
@@ -62,6 +67,7 @@ inquirer
         ]
     }
 ])
+
  
 .then((answers) => {
     if (answers.choice === 'View All Departments') {
@@ -69,6 +75,9 @@ inquirer
             console.table(results);
         }
         )}
+
+
+        
     else if (answers.choice === 'View All Roles') {
         db.query("SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
         , function (err, results) {
@@ -98,7 +107,6 @@ LEFT JOIN employee m ON e.manager_id = m.id;`
         ])
         .then((response) => {
             let department = response.department;
-            console.log(department);
         db.query('INSERT INTO department (name) VALUES (?);', department , function (err, results) {
             console.log(`Added ${department} to table department.`);
         db.query('SELECT * FROM department', function (err, results) {
@@ -134,11 +142,15 @@ LEFT JOIN employee m ON e.manager_id = m.id;`
            
         ])
         .then((response) => {
-            let newRole = (`${response.title}, ${response.department_id}, ${response.salary}`);
-            console.log(`Added ${newRole} to table department.`);
-        db.query('INSERT INTO role (title, department_id, salary) VALUES (?)', newRole , function (err, results) {
-            console.table(results);
+            let newRole = [response.title, response.department_id, response.salary];
+            db.query('INSERT INTO role (title, department_id, salary) VALUES (?,?,?);', newRole , function (err, results) {
+            console.log(`Added ${newRole} to table role.`);
+            
+            db.query('SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id', function (err, results) {
+                console.table(results);
+            });
         }
+        
         )})}
     else if (answers.choice === 'Add an Employee') {
         inquirer
@@ -154,16 +166,10 @@ LEFT JOIN employee m ON e.manager_id = m.id;`
                 message: 'What is the last name of the employee you would like to add?'
             },
             {
-                type: 'list',
+                type: 'input',
                 name: 'role_id',
                 message: 'What is the role id of the employee you would like to add?',
-                choices: [
-                    '1',
-                    '2',
-                    '3',
-                    '4',
-                    '5',
-                ]
+                
             },
             {
                 type: 'list',
@@ -171,42 +177,62 @@ LEFT JOIN employee m ON e.manager_id = m.id;`
                 message: 'What is the manager id of the employee you would like to add? (Select NULL if none)',
                 choices: [
                     '1',
-                    '2',
                     '3',
-                    '4',
                     '5',
-                    'NULL'
-                ]
+                    '7',
+                    '9',
+                    'null'
+                ],
+                filter: (input) => input === 'null' ? null : input,
             },
         ])
         .then((response) => {
-            let newEmployee = (response.first_name, response.last_name, response.role_id, response.manager_id);
-        db.query('INSERT INTO employee SET ?', newEmployee , function (err, results) {
+            let newEmployee = [response.first_name, response.last_name, response.role_id, response.manager_id];
+            console.log(newEmployee)
+        db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);', newEmployee , function (err, results) {
+            db.query(`
+        SELECT e.id AS employee_id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+FROM employee e
+LEFT JOIN role r ON e.role_id = r.id
+LEFT JOIN department d ON r.department_id = d.id
+LEFT JOIN employee m ON e.manager_id = m.id;`
+        , function (err, results) {
             console.table(results);
         }
+        )}
         )})}
     else if (answers.choice === 'Update an Employee Role') {
         inquirer
         .prompt([
             {
-                type: 'choice',
+                type: 'list',
                 name: 'employee_id',
-                message: 'What is the name of the employee you would like to update?',
+                message: 'What is the ID of the employee you would like to update?',
                 choices: [
-                    'John Smith',
-                    'Jane Doe',
-                    'Bob Smith',
-                    'Sally Jones',
-                    'Joe Smith',
-                    'Billy Boi',
-                    'Jonathan Bejarano',
-                    'Mary Jane',
-                    'Jill Smith',
-                    'Jack Smith'
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6',
+                    '7',
+                    '8',
+                    '9',
+                    '10'
+                    // 'John Smith',
+                    // 'Jane Doe',
+                    // 'Bob Smith',
+                    // 'Sally Jones',
+                    // 'Joe Smith',
+                    // 'Billy Boi',
+                    // 'Jonathan Bejarano',
+                    // 'Mary Jane',
+                    // 'Jill Smith',
+                    // 'Jack Smith'
                 ]
             },
             {
-                type: 'choice',
+                type: 'list',
                 name: 'role_id',
                 message: 'What is the new role id of the employee you would like to update?',
                 choices: [
@@ -223,13 +249,35 @@ LEFT JOIN employee m ON e.manager_id = m.id;`
                 ]
             },
         ])
+
         .then((response) => {
-            let updateEmployee = (response.employee_id, response.role_id);
-        db.query('UPDATE employee SET ? WHERE ?', updateEmployee , function (err, results) {
+            // const {employee_id, role_id} = response;
+            // let updateEmployee = [response.employee_id, response.role_id];
+            console.log(response)
+            const employee_id = response.employee_id;
+            const role_id = response.role_id;
+            const sql = `UPDATE employee SET role_id = ${role_id} WHERE employee.id = ${employee_id}`;
+        console.log(sql)
+        db.query(sql, function (err, results) {
+            console.log(`Updated employee ${employee_id} to role ${role_id}.`);
+            db.query(`
+        SELECT e.id AS employee_id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+FROM employee e
+LEFT JOIN role r ON e.role_id = r.id
+LEFT JOIN department d ON r.department_id = d.id
+LEFT JOIN employee m ON e.manager_id = m.id;`
+        , function (err, results) {
             console.table(results);
         }
-        )})}
+        )
+
+
+        }
+        )
+    })}
     
-    });
+});
+   
+
 
 
